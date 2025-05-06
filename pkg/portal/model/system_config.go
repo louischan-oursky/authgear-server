c538c078f9ef186e4c60abf15d0ea40fdebe5dc0
Louis Chan <louischan@oursky.com>
2025-05-06 20:21:01 +0800

    Include license env variables in system config

--- pkg/portal/model/system_config.go
+++ pkg/portal/model/system_config.go
@@ -5,24 +5,27 @@ import (
 )
 
 type SystemConfig struct {
-	AuthgearAppID             string         `json:"authgearAppID"`
-	AuthgearClientID          string         `json:"authgearClientID"`
-	AuthgearEndpoint          string         `json:"authgearEndpoint"`
-	AuthgearWebSDKSessionType string         `json:"authgearWebSDKSessionType"`
-	IsAuthgearOnce            bool           `json:"isAuthgearOnce"`
-	SentryDSN                 string         `json:"sentryDSN,omitempty"`
-	AppHostSuffix             string         `json:"appHostSuffix"`
-	AvailableLanguages        []string       `json:"availableLanguages"`
-	BuiltinLanguages          []string       `json:"builtinLanguages"`
-	Themes                    interface{}    `json:"themes,omitempty"`
-	Translations              interface{}    `json:"translations,omitempty"`
-	SearchEnabled             bool           `json:"searchEnabled"`
-	AuditLogEnabled           bool           `json:"auditLogEnabled"`
-	AnalyticEnabled           bool           `json:"analyticEnabled"`
-	AnalyticEpoch             *timeutil.Date `json:"analyticEpoch,omitempty"`
-	GitCommitHash             string         `json:"gitCommitHash,omitempty"`
-	GTMContainerID            string         `json:"gtmContainerID,omitempty"`
-	UIImplementation          string         `json:"uiImplementation,omitempty"`
-	UISettingsImplementation  string         `json:"uiSettingsImplementation,omitempty"`
-	ShowCustomSMSGateway      bool           `json:"showCustomSMSGateway,omitempty"`
+	AuthgearAppID               string         `json:"authgearAppID"`
+	AuthgearClientID            string         `json:"authgearClientID"`
+	AuthgearEndpoint            string         `json:"authgearEndpoint"`
+	AuthgearWebSDKSessionType   string         `json:"authgearWebSDKSessionType"`
+	IsAuthgearOnce              bool           `json:"isAuthgearOnce"`
+	AuthgearOnceLicenseKey      string         `json:"authgearOnceLicenseKey,omitempty"`
+	AuthgearOnceLicenseExpireAt string         `json:"authgearOnceLicenseExpireAt,omitempty"`
+	AuthgearOnceLicenseeEmail   string         `json:"authgearOnceLicenseeEmail,omitempty"`
+	SentryDSN                   string         `json:"sentryDSN,omitempty"`
+	AppHostSuffix               string         `json:"appHostSuffix"`
+	AvailableLanguages          []string       `json:"availableLanguages"`
+	BuiltinLanguages            []string       `json:"builtinLanguages"`
+	Themes                      interface{}    `json:"themes,omitempty"`
+	Translations                interface{}    `json:"translations,omitempty"`
+	SearchEnabled               bool           `json:"searchEnabled"`
+	AuditLogEnabled             bool           `json:"auditLogEnabled"`
+	AnalyticEnabled             bool           `json:"analyticEnabled"`
+	AnalyticEpoch               *timeutil.Date `json:"analyticEpoch,omitempty"`
+	GitCommitHash               string         `json:"gitCommitHash,omitempty"`
+	GTMContainerID              string         `json:"gtmContainerID,omitempty"`
+	UIImplementation            string         `json:"uiImplementation,omitempty"`
+	UISettingsImplementation    string         `json:"uiSettingsImplementation,omitempty"`
+	ShowCustomSMSGateway        bool           `json:"showCustomSMSGateway,omitempty"`
 }
